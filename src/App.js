4a7a5430fa23b3d0ffc74d520df71255f5ecfad2
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-06-07 13:41:58 -0300

    v1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+import freeCodeCampLogo from './imagenes/logo.png'
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className="Aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img  
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo' 
+        />
+      </div>
+
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
